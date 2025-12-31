0061e44e96efbe2655fa4287b8a626bdd06998b9
ebisa <225586468+ebisa22@users.noreply.github.com>
2025-12-30 22:41:56 -0800

    setting up gitignore file

--- webpack.common.js
+++ webpack.common.js
@@ -10,8 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js", //content hash use hash and the hash changes if something in the module changes.
     assetModuleFilename: "[name][ext]", //keeps the same name for images when compiled from src to dist
-    clean: true,
-    publicPath: "./",
+    clean: true, 
   },
   plugins: [
     new HtmlWebpackPlugin({
